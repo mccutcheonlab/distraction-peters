a62c9abccaff7744ab7ca2fdef6a30d05bda1ec3
KP <kp259@le.ac.uk>
2018-10-23 17:25:52 +0100

    calculate ratdata

--- RasterPlots.py
+++ RasterPlots.py
@@ -5,9 +5,82 @@ Created on Sat Oct 20 21:17:27 2018
 
 @author: u1490431
 """
+# Taken from Ch4 analysis_licking - currently contains a lot of redundant code 
+
+# On licking day (modelled distractors --> all rats)
+
+# On distraction day (real distractors)
+# On habituation day (real distractors but less distracted)
+
+# Ordered then not ordered (check how you will change the colours)
+ 
+
+## lick day (modelled distractors)
+
+## 
+for filename in TDTfiles_thph_lick:
+    
+    file = TDTfilepath + filename
+    ratdata = loadmatfile(file)
+    allRatBlue.append(ratdata['blue'])
+    allRatUV.append(ratdata['uv'])
+    allRatFS.append(ratdata['fs'])
+    allRatLicks.append(ratdata['licks'])
+    burstanalysis = lickCalc(ratdata['licks'], offset=ratdata['licks_off'])
+    burstList = burstanalysis['bLicks'] # n licks per burst 
+    runList = burstanalysis['rLicks'] # n licks per run
+    burstListTimes = burstanalysis['bStart'] # Actual times of start of runs  
+    runListTimes = burstanalysis['rStart'] # Actual times of start of bursts 
+    allBursts.append(burstList)
+    allRuns.append(runList)
+    allRunTimes.append(runListTimes)
+    allBurstsTimes.append(burstListTimes)
+
+    allRatDistractorsMOD.append(ratdata['distractors'])
+    allRatDistractedMOD.append(ratdata['distracted'])
+    allRatNotDistractedMOD.append(ratdata['notdistracted'])
+    print(ratdata['distractors'])
 
 
 
+#!!! There are no distractors!!!!! 
+
+
+    indices1 = []       
+    for index, value in enumerate(ratdata['distractors']):
+        a = np.where(ratdata['licks'] == value) 
+        indices1.append(a)       
+    
+    pdps = []
+    for tupl in indices1:
+        i = tupl[0][0]
+        if i+1 < len(ratdata['licks']):
+            
+            pdp = (ratdata['licks'][i+1] - ratdata['licks'][i])
+            pdps.append(pdp)    
+    
+   # pdps.append(0) ## Add in a check, if PDPs len does not equal len(distractors)
+    figure12 = plt.figure(figsize=(6,6))
+    ax6 = plt.subplot(111)
+    ax6.spines['right'].set_visible(False)
+    ax6.xaxis.set_visible(False)
+    ax6.spines['top'].set_visible(False)
+    ax6.spines['bottom'].set_visible(False)
+    ax6.set(ylabel = 'Trials')
+    ax6.yaxis.label.set_size(14)
+    
+    scale = 1
+    scalebar = 1
+    yrange = ax6.get_ylim()[1] - ax6.get_ylim()[0]
+    scalebary = (yrange / 10) + ax6.get_ylim()[0]
+    scalebarx = [ax6.get_xlim()[1] - scalebar, ax6.get_xlim()[1]]
+    ax6.plot(scalebarx, [scalebary, scalebary], c='k', linewidth=2)
+    ax6.text((scalebarx[0] + (scalebar/2)), scalebary-(yrange/50), str(scale) +' s', ha='center',va='top', **Calibri, **Size) 
+    
+    
+    rasterPlot = distractionrasterFig(ax6, ratdata['distractors'], ratdata['licks'], pre=1, post=10, sortevents=pdps, sortdirection='dec')
+    #figure12.savefig('/Volumes/KPMSB352/PHOTOMETRY MMIN18/PDF figures/RasterLickDay2.3.pdf') 
+        
 
 # Post distraction pause rater plot 
 
@@ -43,7 +116,9 @@ def distractionrasterFig(ax, timelock, events,
         xvals = [x for x in trial]  
         yvals = [1+ith] * len(xvals)
         
-        #if ith<40:  # 26 if ascending
+       
+        
+        #if ith<40:  # 26 if ascending --> MANUALLY CHECKED? --> could check the len of PDP if not ordered
             #ax.scatter(xvals, yvals, marker='.', color='b')
             
         #else:
@@ -68,8 +143,6 @@ for tupl in indices1:
         pdp = (examplerat['licks'][i+1] - examplerat['licks'][i])
         pdps.append(pdp)    
 
-# Check the PDPs first one is very long?Yes it is 
-#
 pdps.append(0)        
 figure12 = plt.figure(figsize=(6,6))
 ax6 = plt.subplot(111)
@@ -88,6 +161,7 @@ scalebarx = [ax6.get_xlim()[1] - scalebar, ax6.get_xlim()[1]]
 ax6.plot(scalebarx, [scalebary, scalebary], c='k', linewidth=2)
 ax6.text((scalebarx[0] + (scalebar/2)), scalebary-(yrange/50), str(scale) +' s', ha='center',va='top', **Calibri, **Size) 
 
+
 rasterPlot = distractionrasterFig(ax6, examplerat['distractors'], examplerat['licks'], pre=1, post=10, sortevents=pdps, sortdirection='dec')
 #figure12.savefig('/Volumes/KPMSB352/PHOTOMETRY MMIN18/PDF figures/RasterLickDay2.3.pdf') 
 
@@ -103,4 +177,20 @@ rasterPlot = distractionrasterFig(ax6, examplerat['distractors'], examplerat['li
 #    
 #   s = 1  
     
-    
+#
+#Do I need to check if the last lick is a distractor (doesnt have a pdp) 
+#What to do in this case?
+#
+#Check the lengths of PDPs list and the  
+
+
+for index, value in enumerate(pdps):
+    if value < 1:
+        print(value)
+        
+        
+How to get the NON sorted lengths of PDPs one by one, with a logical index 
+THEN use the 0/1 to determine which colour to draw the plots
+
+For index and value in the sortevents, is value < 1 do the plotting as
+usual, if value > 1 make the plot a different colour (could add as a parameter)
