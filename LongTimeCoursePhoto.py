fee34d487fc9cdbbbd724517fdab3dd08945f15e
Kate Peters <kp259@le.ac.uk>
2019-01-09 19:38:58 +0000

    Instructions added as comments

---
+++ LongTimeCoursePhoto.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  9 19:09:07 2019
+
+@author: kate
+"""
+
+## Functions and code to creat the long time course figures for distraction paper
+
+# Step 1 - run the all functions file 
+# Step 2 - run the Ch4_analysis_licking and Ch4_analysis_distraction files 
+# Step 3 - change the snipper function to make longer snips and add this to this file
+# Step 4 - copy over only the code that is needed from the licking and distraciton file 
+#        - to make the long time course figures 
+
+#! ! ! Decide if you are going to use data from one representative rats or all of the animals 
+        # Check back to MMIN18 and see whether you used group or individual data 
+        # Maybe only need the blue signal or maybe also violet is useful 
+
+# Step 5 - make and save long timecourse figures 
+        
+        
+# Step 6 - subset the required data for the barscatter plots, decide which info is important 
+        
+# Step 7 - calculate peaks within time region and look at these statistically 
+        
+        
+# Step 8 - check which figures and statistics from thesis / and corrections (time bins)
+#        - will be used
+
+# Step 9 - make barscatter of 'probability' of distraction in 10 min time bins 
+#        - averaged for all of the rats (make the percent for each and then average)
+#        - show statistically the lack of habituaiton effect within the session 
+        
+        
+
+## Step 10 - is it possible to make barscatters for average post distraction pauses 
+#          - or other measure median then bin these into 10 mins as with percent distracted?
+        
+        
