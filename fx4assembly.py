93646edf8cf7a470b34de62005bf6466184cb13d
Jaime McCutcheon <mccutcheon.james@gmail.com>
2020-02-05 15:24:07 +0100

    Updated script to use tdt to extract from raw data files

---
+++ fx4assembly.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  5 12:39:35 2020
+
+@author: admin
+"""
+import numpy as np
+import scipy.signal as sig
+
+def correctforbaseline(blue, uv):
+    pt = len(blue)
+    X = np.fft.rfft(uv, pt)
+    Y = np.fft.rfft(blue, pt)
+    Ynet = Y-X
+
+    datafilt = np.fft.irfft(Ynet)
+
+    datafilt = sig.detrend(datafilt)
+
+    b, a = sig.butter(9, 0.012, 'low', analog=True)
+    datafilt = sig.filtfilt(b, a, datafilt)
+    
+    return datafilt
+
+def remcheck(val, range1, range2):
+    # function checks whether value is within range of two decimels
+    if (range1 < range2):
+        if (val > range1) and (val < range2):
+            return True
+        else:
+            return False
+    else:
+        if (val > range1) or (val < range2):
+            return True
+        else:
+            return False
+
+def distractionCalc2(licks, pre=1, post=1):
+    licks = np.insert(licks, 0, 0)
+    b = 0.001
+    d = []
+    idx = 3
+    
+    while idx < len(licks):
+        if licks[idx]-licks[idx-2] < 1 and remcheck(b, licks[idx-2] % 1, licks[idx] % 1) == False:
+                d.append(licks[idx])
+                b = licks[idx] % 1
+                idx += 1
+                try:
+                    while licks[idx]-licks[idx-1] < 1:
+                        b = licks[idx] % 1
+                        idx += 1
+                except IndexError:
+                    pass
+        else:
+            idx +=1
+            
+    if d[-1] > 3599:
+        d = d[:-1]
+    
+    return d
+
+def distractedOrNot(distractors, licks):
+    distracted = []
+    notdistracted = []
+    lickList = []
+    for l in licks:
+        lickList.append(l)
+    
+
+    for index, distractor in enumerate(distractors):
+        if distractor in licks:
+
+            ind = lickList.index(distractor)
+            try:
+                if (licks[ind+1] - licks[ind]) > 1:
+                    distracted.append(licks[ind])
+                else:
+                    if (licks[ind+1] - licks[ind]) < 1:
+                        notdistracted.append(licks[ind])
+            except IndexError:
+                print('last lick was a distractor!!!')
+                distracted.append(licks[ind])
+
+    return(distracted, notdistracted)
